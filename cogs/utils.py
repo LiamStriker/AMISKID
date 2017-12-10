8b49887a1f3a834868349cdefaf791651865bd43
LiamStriker <33391338+LiamStriker@users.noreply.github.com>
2017-12-11 00:57:00 +0530

    skidding

---
+++ cogs/utils.py
@@ -0,0 +1,201 @@
+'''
+Actual Cog from Selfbot, edited by Quanta#5556 (N)
+'''
+import discord
+from discord.ext import commands
+from discord.ext.commands import TextChannelConverter
+from contextlib import redirect_stdout
+from ext.utility import load_json
+from urllib.parse import quote as uriquote
+from lxml import etree
+from ext import fuzzy
+from ext import embedtobox
+from PIL import Image
+import unicodedata
+import traceback
+import textwrap
+import aiohttp
+import inspect
+import asyncio
+import time
+import re
+import io
+import os
+import random
+
+class Utility:
+    '''Useful commands to make your life easier'''
+    def __init__(self, bot):
+        self.bot = bot
+        self.lang_conv = load_json('data/langs.json')
+        self._last_embed = None
+        self._rtfm_cache = None
+        self._last_google = None
+        self._last_result = None
+
+    async def is_owner(ctx):
+       return ctx.author.id == 300944772971888640
+
+
+    @commands.command(name='logout')
+    @commands.check(is_owner)
+    async def _logout(self, ctx):
+        '''
+        Shuts down the selfbot,
+        equivalent to a restart if you are hosting the bot on heroku.
+        '''
+        await ctx.send('`Selfbot Logging out...`')
+        await self.bot.logout()
+
+    @commands.command(name='help')
+    async def help(self, ctx):
+        help_cmd = """
+```
+1.opuser :
+    Gives The User Info.E.g. opuser or opuser @mention
+
+2.opserver :
+    Gives The Server Info
+
+3.ACTIONS ~
+
+    opkiss <tag person>
+        kiss Someone
+    opbite <tag person>
+        Bite Someone :3
+    ophug <tag person>
+        Hug Someone
+    opslap <tag person>
+         Slap Someone
+
+4.opcmd <name> :
+    Personal Commands. E.g. opcmd mika
+
+5.opavatar and opservericon :
+    Get The Avatar image
+
+6.opabout :
+    Details About Me (^~^)
+
+7.opinvit :
+    Invite Me To Your Server (^~^)
+
+8.Ami/ami :
+    Talk to me (^~^) start your sentence with Ami/ami
+
+9.op8ball :
+    See your future
+
+10.opserverlist :
+     Shows the list of servers I am in (^~^)
+
+**Music Commands:**
+ Music Prefix : /
+
+1. /summon: before using.
+
+2. /play [song]: play the song.
+
+3. /repeat: toggle repeat modes.
+
+4. /queue: songs in queue.
+
+5. /volume [value]: set volume.
+
+6. /promote [number]: put song to top.
+
+7. /disconnect [always do.]
+ ```
+        """
+        await ctx.send("**PREFIX = op** ***AND*** **MUSIC PREFIX = /*** "+help_cmd)
+
+    @commands.command(pass_context=True, hidden=True, name='eval')
+    @commands.check(is_owner)
+    async def _eval(self, ctx, *, body: str):
+        """Evaluates python code"""
+
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self._last_result,
+            'source': inspect.getsource
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        #await self.edit_to_codeblock(ctx, body)
+        stdout = io.StringIO()
+        err = out = None
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+            return await err.add_reaction('\u2049')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            if ret is None:
+                if value:
+                    try:
+                        out = await ctx.send(f'```py\n{value}\n```')
+                    except:
+                        paginated_text = ctx.paginate(value)
+                        for page in paginated_text:
+                            if page == paginated_text[-1]:
+                                out = await ctx.send(f'```py\n{page}\n```')
+                                break
+                            await ctx.send(f'```py\n{page}\n```')
+            else:
+                self._last_result = ret
+                try:
+                    out = await ctx.send(f'```py\n{value}{ret}\n```')
+                except:
+                    paginated_text = ctx.paginate(f"{value}{ret}")
+                    for page in paginated_text:
+                        if page == paginated_text[-1]:
+                            out = await ctx.send(f'```py\n{page}\n```')
+                            break
+                        await ctx.send(f'```py\n{page}\n```')
+
+        if out:
+            await out.add_reaction('\u2705') #tick
+        if err:
+            await err.add_reaction('\u2049') #x
+
+
+    #async def edit_to_codeblock(self, ctx, body):
+        #msg = f'{ctx.prefix}eval\n```py\n{body}\n```'
+        #await ctx.message.edit(content=msg)
+
+
+    def cleanup_code(self, content):
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+
+        # remove `foo`
+        return content.strip('` \n')
+
+    def get_syntax_error(self, e):
+        if e.text is None:
+            return f'```py\n{e.__class__.__name__}: {e}\n```'
+        return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
+
+def setup(bot):
+    bot.add_cog(Utility(bot))
