54c35b75b039c297cf7aeffadeefcf185e52be80
EC <saeqaed@yahoo.com>
2018-06-11 14:18:39 +0500

    Update lvls.py

--- cogs/lvls.py
+++ cogs/lvls.py
@@ -4,24 +4,24 @@ import random
 import json
 
 class levels:
-    def __init__:
+    def __init__(self, bot):
         self.bot = bot
-    lvls = {}
+        self.lvls = {}
     async def on_message(self, message):
-        id = ctx.message.author.id
-        if id not in lvls:
-            lvls[id] = {'name' : f'{ctx.author.name}'}, 'xp' : 0, 'level' : {'lvlNo' : 1, 'totalxp' : 100}}
-        else:
-            lvls[id]['xp'] += random.randint(10, 50)
-            if lvls[id]['xp'] >= lvls[id]['level']['totalexp']:
-                lvls[id]['xp'] = 0
-                lvls[id]['level']['lvlNo'] += 1
-                lvls[id]['level']['totalxp'] += lvls[id]['level']['totalxp']/2
-                await message.channel.send(content = f"congrats lvls[id]['name'] you advanced to level lvls[id]['level']['lvlNo']")
-        data = json.dumps(lvls)
+        id = message.author.id
+        if id not in self.lvls and message.author.bot == False:
+            self.lvls[id] = {'name' : message.author.name, 'xp' : 0, 'level' : {'lvlNo' : 1, 'totalxp' : 100}}
+        elif message.author.bot == False:
+            self.lvls[id]['xp'] += random.randint(10, 50)
+            if self.lvls[id]['xp'] >= self.lvls[id]['level']['totalxp']:
+                self.lvls[id]['xp'] = 0
+                self.lvls[id]['level']['lvlNo'] += 1
+                self.lvls[id]['level']['totalxp'] += self.lvls[id]['level']['totalxp']/2
+                #await message.channel.send(content = f"congrats {self.lvls[id]['name']} you advanced to level {self.lvls[id]['level']['lvlNo']}")
+        data = json.dumps(self.lvls)
         file = open('data/lvls.json', 'w')
         file.write(data)
         file.close()
         
 def setup(bot):
-    bot.add_cog(lvls(bot))
+    bot.add_cog(levels(bot))
